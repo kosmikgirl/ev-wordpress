0f465dd6c6503ea9cb739d3e44b62cade659aac3
Juliana Rocha <rochajulianacarolina@gmail.com>
2024-08-09 18:00:28 +0100

    Last changes

---
+++ wp-content/plugins/duplicate-page/duplicatepage.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function plugin_basename() in /Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/duplicate-page/duplicatepage.php:13
+Stack trace:
+#0 {main}
+  thrown in <b>/Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/duplicate-page/duplicatepage.php</b> on line <b>13</b><br>
---
+++ wp-content/plugins/duplicate-page/inc/admin-settings.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function current_user_can() in /Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/duplicate-page/inc/admin-settings.php:1
+Stack trace:
+#0 {main}
+  thrown in <b>/Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/duplicate-page/inc/admin-settings.php</b> on line <b>1</b><br>
