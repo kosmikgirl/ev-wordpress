c7a986ae105120f9bdbbf95a5a47f4b70de9d4e2
Juliana Rocha <rochajulianacarolina@gmail.com>
2024-08-10 08:58:42 +0100

    Last changes

--- wp-content/plugins/learnpress/inc/admin/settings/class-lp-settings-general.php
+++ wp-content/plugins/learnpress/inc/admin/settings/class-lp-settings-general.php
@@ -1,5 +1,5 @@
 <br>
-<b>Fatal error</b>:  Uncaught Error: Class &quot;LP_Abstract_Settings_Page&quot; not found in /Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/admin/settings/class-lp-settings-general.php:10
+<b>Fatal error</b>:  Uncaught Error: Class &quot;LP_Abstract_Settings_Page&quot; not found in /Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/admin/settings/class-lp-settings-general.php:12
 Stack trace:
 #0 {main}
-  thrown in <b>/Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/admin/settings/class-lp-settings-general.php</b> on line <b>10</b><br>
+  thrown in <b>/Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/admin/settings/class-lp-settings-general.php</b> on line <b>12</b><br>
--- wp-content/plugins/learnpress/inc/admin/settings/class-lp-settings-permalink.php
+++ wp-content/plugins/learnpress/inc/admin/settings/class-lp-settings-permalink.php
@@ -1,5 +1,5 @@
 <br>
-<b>Fatal error</b>:  Uncaught Error: Class &quot;LP_Abstract_Settings_Page&quot; not found in /Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/admin/settings/class-lp-settings-permalink.php:11
+<b>Fatal error</b>:  Uncaught Error: Class &quot;LP_Abstract_Settings_Page&quot; not found in /Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/admin/settings/class-lp-settings-permalink.php:13
 Stack trace:
 #0 {main}
-  thrown in <b>/Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/admin/settings/class-lp-settings-permalink.php</b> on line <b>11</b><br>
+  thrown in <b>/Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/admin/settings/class-lp-settings-permalink.php</b> on line <b>13</b><br>
