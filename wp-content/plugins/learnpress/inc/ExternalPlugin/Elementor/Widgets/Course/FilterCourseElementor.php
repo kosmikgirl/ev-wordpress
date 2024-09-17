8cb03ecda34b9c46fa7ae1fac955d0f0c1ae0f3a
Juliana Rocha <rochajulianacarolina@gmail.com>
2024-09-17 10:36:55 +0100

    About changes

--- wp-content/plugins/learnpress/inc/ExternalPlugin/Elementor/Widgets/Course/FilterCourseElementor.php
+++ wp-content/plugins/learnpress/inc/ExternalPlugin/Elementor/Widgets/Course/FilterCourseElementor.php
@@ -1,5 +1,5 @@
 <br>
-<b>Fatal error</b>:  Uncaught Error: Class &quot;LearnPress\ExternalPlugin\Elementor\LPElementorWidgetBase&quot; not found in /Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/ExternalPlugin/Elementor/Widgets/Course/FilterCourseElementor.php:19
+<b>Fatal error</b>:  Uncaught Error: Class &quot;LearnPress\ExternalPlugin\Elementor\LPElementorWidgetBase&quot; not found in /Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/ExternalPlugin/Elementor/Widgets/Course/FilterCourseElementor.php:20
 Stack trace:
 #0 {main}
-  thrown in <b>/Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/ExternalPlugin/Elementor/Widgets/Course/FilterCourseElementor.php</b> on line <b>19</b><br>
+  thrown in <b>/Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/ExternalPlugin/Elementor/Widgets/Course/FilterCourseElementor.php</b> on line <b>20</b><br>
--- wp-content/plugins/learnpress/inc/lp-deprecated.php
+++ wp-content/plugins/learnpress/inc/lp-deprecated.php
@@ -1,5 +1,5 @@
 <br>
-<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in /Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/lp-deprecated.php:224
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in /Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/lp-deprecated.php:225
 Stack trace:
 #0 {main}
-  thrown in <b>/Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/lp-deprecated.php</b> on line <b>224</b><br>
+  thrown in <b>/Users/julziten/Local Sites/estupendavibra/app/public/wp-content/plugins/learnpress/inc/lp-deprecated.php</b> on line <b>225</b><br>
